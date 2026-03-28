c7547bde8a12abc5fec243cc9997a04a888676d4
Jim Hayes <hayesjm@gmail.com>
2026-03-27 20:28:52 -0500

    refactor(grid-controls): bind GridControlsPanel to SlicerProject

--- lib/widgets/panels/grid_controls_panel.dart
+++ lib/widgets/panels/grid_controls_panel.dart
@@ -1,252 +1,219 @@
 import 'package:flutter/material.dart';
+import '../../model/slicer_project.dart';
 import '../controls/mode_button_row.dart';
 import '../controls/slider_number_row.dart';
 import 'titled_panel.dart';
-import '../../model/slicer_project.dart';
 
-class GridControlsPanel extends StatefulWidget {
+class GridControlsPanel extends StatelessWidget {
   final SlicerProject project;
+
   const GridControlsPanel({
     super.key,
     required this.project,
   });
 
   @override
-  State<GridControlsPanel> createState() => _GridControlsPanelState();
-}
-
-class _GridControlsPanelState extends State<GridControlsPanel> {
-  String _gridMode = 'cart';
-  String _lineType = 'none';
-  String _gridColor = 'black';
-
-  double _isoAngle = 0;
-  double _rotation = 0;
-  double _gridSize = 1.0;
-  double _stroke = 1.0;
-  double _offsetX = 0;
-  double _offsetY = 0;
-
-  @override
   Widget build(BuildContext context) {
-    return TitledPanel(
-      title: 'Grid Controls',
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          ModeButtonRow<String>(
-            label: 'Grid Mode',
-            value: _gridMode,
-            options: [
-              ModeOption(
-                value: 'cart',
-                iconWidget: Image.asset(
-                  'assets/icons/gm-cart.png',
-                  width: 21,
-                  height: 21,
-                ),
+    return ListenableBuilder(
+      listenable: project,
+      builder: (context, _) {
+        return TitledPanel(
+          title: 'Grid Controls',
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ModeButtonRow<String>(
+                label: 'Grid Mode',
+                value: project.gridMode,
+                options: [
+                  ModeOption(
+                    value: 'cart',
+                    iconWidget: Image.asset(
+                      'assets/icons/gm-cart.png',
+                      width: 21,
+                      height: 21,
+                    ),
+                  ),
+                  ModeOption(
+                    value: 'iso',
+                    iconWidget: Image.asset(
+                      'assets/icons/gm-iso.png',
+                      width: 21,
+                      height: 21,
+                    ),
+                  ),
+                ],
+                onChanged: (mode) {
+                  project.gridMode = mode;
+                },
               ),
-              ModeOption(
-                value: 'iso',
-                iconWidget: Image.asset(
-                  'assets/icons/gm-iso.png',
-                  width: 21,
-                  height: 21,
-                ),
+
+              ModeButtonRow<String>(
+                label: 'Line Type',
+                value: project.lineType,
+                options: [
+                  ModeOption(
+                    value: 'none',
+                    iconWidget: Image.asset(
+                      'assets/icons/gl-none.png',
+                      width: 21,
+                      height: 21,
+                    ),
+                  ),
+                  ModeOption(
+                    value: 'line',
+                    iconWidget: Image.asset(
+                      'assets/icons/gl-solid.png',
+                      width: 21,
+                      height: 21,
+                    ),
+                  ),
+                  ModeOption(
+                    value: 'dash',
+                    iconWidget: Image.asset(
+                      'assets/icons/gl-dash.png',
+                      width: 21,
+                      height: 21,
+                    ),
+                  ),
+                  ModeOption(
+                    value: 'corner',
+                    iconWidget: Image.asset(
+                      'assets/icons/gl-corner.png',
+                      width: 21,
+                      height: 21,
+                    ),
+                  ),
+                ],
+                onChanged: (mode) {
+                  project.lineType = mode;
+                },
+              ),
+
+              ModeButtonRow<String>(
+                label: 'Grid Color',
+                value: project.gridColor,
+                options: [
+                  ModeOption(
+                    value: 'black',
+                    iconWidget: Image.asset(
+                      'assets/icons/gc-black.png',
+                      width: 21,
+                      height: 21,
+                    ),
+                  ),
+                  ModeOption(
+                    value: 'gray',
+                    iconWidget: Image.asset(
+                      'assets/icons/gc-gray.png',
+                      width: 21,
+                      height: 21,
+                    ),
+                  ),
+                  ModeOption(
+                    value: 'white',
+                    iconWidget: Image.asset(
+                      'assets/icons/gc-white.png',
+                      width: 21,
+                      height: 21,
+                    ),
+                  ),
+                ],
+                onChanged: (mode) {
+                  project.gridColor = mode;
+                },
+              ),
+
+              SliderNumberRow(
+                label: 'Angle',
+                value: project.gridMode == 'iso' ? project.isoAngle : 0,
+                min: project.gridMode == 'iso' ? 15 : 0,
+                max: project.gridMode == 'iso' ? 45 : 0,
+                defaultValue: 0,
+                divisions: project.gridMode == 'iso' ? 300 : 1,
+                decimals: 1,
+                units: '°',
+                onChanged: project.gridMode == 'iso'
+                    ? (value) {
+                        project.isoAngle = value;
+                      }
+                    : null,
+              ),
+
+              SliderNumberRow(
+                label: 'Rotation',
+                value: project.gridRotation,
+                min: -180,
+                max: 180,
+                defaultValue: 0,
+                divisions: 360,
+                decimals: 0,
+                units: '°',
+                onChanged: (value) {
+                  project.gridRotation = value;
+                },
+              ),
+
+              SliderNumberRow(
+                label: 'Size',
+                value: project.gridSizeIn,
+                min: 0.25,
+                max: 3.00,
+                defaultValue: 1.00,
+                divisions: 275,
+                decimals: 2,
+                units: '"',
+                onChanged: (value) {
+                  project.gridSizeIn = value;
+                },
+              ),
+
+              SliderNumberRow(
+                label: 'Stroke',
+                value: project.gridStroke,
+                min: 0.1,
+                max: 5.0,
+                defaultValue: 1.0,
+                divisions: 49,
+                decimals: 1,
+                units: 'p',
+                onChanged: (value) {
+                  project.gridStroke = value;
+                },
+              ),
+
+              SliderNumberRow(
+                label: 'Offset X',
+                value: project.gridOffsetX,
+                min: -1.0,
+                max: 1.0,
+                defaultValue: 0,
+                divisions: 200,
+                decimals: 2,
+                units: 'x',
+                onChanged: (value) {
+                  project.gridOffsetX = value;
+                },
+              ),
+
+              SliderNumberRow(
+                label: 'Offset Y',
+                value: project.gridOffsetY,
+                min: -1.0,
+                max: 1.0,
+                defaultValue: 0,
+                divisions: 200,
+                decimals: 2,
+                units: 'y',
+                onChanged: (value) {
+                  project.gridOffsetY = value;
+                },
               ),
             ],
-            onChanged: (mode) {
-              setState(() {
-                _gridMode = mode;
-
-                if (_gridMode != 'iso') {
-                  _isoAngle = 0;
-                } else {
-                  if (_isoAngle < 15) _isoAngle = 30; // reasonable default
-                }
-              });
-            },
           ),
-
-          ModeButtonRow<String>(
-            label: 'Line Type',
-            value: _lineType,
-            options: [
-              ModeOption(
-                value: 'none',
-                iconWidget: Image.asset(
-                  'assets/icons/gl-none.png',
-                  width: 21,
-                  height: 21,
-                ),
-              ),
-              ModeOption(
-                value: 'line',
-                iconWidget: Image.asset(
-                  'assets/icons/gl-solid.png',
-                  width: 21,
-                  height: 21,
-                ),
-              ),
-              ModeOption(
-                value: 'dash',
-                iconWidget: Image.asset(
-                  'assets/icons/gl-dash.png',
-                  width: 21,
-                  height: 21,
-                ),
-              ),
-              ModeOption(
-                value: 'corner',
-                iconWidget: Image.asset(
-                  'assets/icons/gl-corner.png',
-                  width: 21,
-                  height: 21,
-                ),
-              ),
-            ],
-            onChanged: (mode) {
-              setState(() {
-                _lineType = mode;
-              });
-            },
-          ),
-
-          ModeButtonRow<String>(
-            label: 'Grid Color',
-            value: _gridColor,
-            options: [
-              ModeOption(
-                value: 'black',
-                iconWidget: Image.asset(
-                  'assets/icons/gc-black.png',
-                  width: 21,
-                  height: 21,
-                ),
-              ),
-              ModeOption(
-                value: 'gray',
-                iconWidget: Image.asset(
-                  'assets/icons/gc-gray.png',
-                  width: 21,
-                  height: 21,
-                ),
-              ),
-              ModeOption(
-                value: 'white',
-                iconWidget: Image.asset(
-                  'assets/icons/gc-white.png',
-                  width: 21,
-                  height: 21,
-                ),
-              ),
-            ],
-            onChanged: (mode) {
-              setState(() {
-                _gridColor = mode;
-              });
-            },
-          ),
-
-          SliderNumberRow(
-            label: 'Angle',
-            value: _gridMode == 'iso' ? _isoAngle : 0,
-            min: _gridMode == 'iso' ? 15 : 0,
-            max: _gridMode == 'iso' ? 45 : 0,
-            defaultValue: 0,
-            divisions: _gridMode == 'iso' ? 300 : 1,
-            decimals: 1,
-            units: '',
-            onChanged: _gridMode == 'iso'
-                ? (value) {
-                    setState(() {
-                      _isoAngle = value;
-                    });
-                  }
-                : null,
-          ),
-          SliderNumberRow(
-            label: 'Rotation',
-            value: _rotation,
-            min: -180,
-            max: 180,
-            defaultValue: 0,
-            divisions: 360,
-            decimals: 0,
-            units: '',
-            onChanged: (value) {
-              setState(() {
-                _rotation = value;
-              });
-            },
-          ),
-
-          SliderNumberRow(
-            label: 'Size',
-            value: _gridSize,
-            min: 0.25,
-            max: 3.00,
-            defaultValue: 1.00,
-            divisions: 275,
-            decimals: 2,
-            units: '',
-            onChanged: (value) {
-              setState(() {
-                _gridSize = value;
-              });
-            },
-          ),
-
-          SliderNumberRow(
-            label: 'Stroke',
-            value: _stroke,
-            min: 1.0,
-            max: 5.0,
-            defaultValue: 1.0,
-            divisions: 9,
-            decimals: 1,
-            units: '',
-            onChanged: (value) {
-              setState(() {
-                _stroke = value;
-              });
-            },
-          ),
-
-          SliderNumberRow(
-            label: 'Offset X',
-            value: _offsetX,
-            min: -100.0,
-            max: 100.0,
-            defaultValue: 0,
-            divisions: 200,
-            decimals: 1,
-            units: '',
-            onChanged: (value) {
-              setState(() {
-                _offsetX = value;
-              });
-            },
-          ),
-
-          SliderNumberRow(
-            label: 'Offset Y',
-            value: _offsetY,
-            min: -100.0,
-            max: 100.0,
-            defaultValue: 0,
-            divisions: 200,
-            decimals: 1,
-            units: '',
-            onChanged: (value) {
-              setState(() {
-                _offsetY = value;
-              });
-            },
-          ),
-        ],
-      ),
+        );
+      },
     );
   }
 }
--- lib/widgets/panels/image_controls_panel.dart
+++ lib/widgets/panels/image_controls_panel.dart
@@ -1,22 +1,22 @@
 import 'package:flutter/material.dart';
+import '../../model/slicer_project.dart';
 import '../../services/image_loader.dart';
 import '../controls/file_load_row.dart';
 import '../controls/info_value_row.dart';
 import '../controls/labeled_switch_row.dart';
 import '../controls/slider_number_row.dart';
 import 'titled_panel.dart';
-import '../../model/slicer_project.dart';
 
 class ImageControlsPanel extends StatefulWidget {
+  final SlicerProject project;
   final LoadedImageFile? loadedImage;
   final ValueChanged<LoadedImageFile?> onImageLoaded;
-  final SlicerProject project;
 
   const ImageControlsPanel({
     super.key,
+    required this.project,
     required this.loadedImage,
     required this.onImageLoaded,
-    required this.project,
   });
 
   @override
@@ -24,16 +24,6 @@ class ImageControlsPanel extends StatefulWidget {
 }
 
 class _ImageControlsPanelState extends State<ImageControlsPanel> {
-  double _zoom = 100;
-  double _offsetX = 0;
-  double _offsetY = 0;
-  double _brightness = 100;
-  double _contrast = 100;
-  double _saturation = 100;
-  double _gamma = 1.00;
-  bool _maintainAspectRatio = true;
-  
-
   Future<void> _handleLoadImage() async {
     final loaded = await ImageLoaderService.pickImage();
     if (loaded == null) return;
@@ -42,88 +32,140 @@ class _ImageControlsPanelState extends State<ImageControlsPanel> {
 
   @override
   Widget build(BuildContext context) {
-    final imageName = widget.loadedImage?.fileName ?? 'No file selected';
-    final sourceSize = widget.loadedImage == null
-        ? '—'
-        : '${widget.loadedImage!.pixelWidth} × ${widget.loadedImage!.pixelHeight}';
+    return ListenableBuilder(
+      listenable: widget.project,
+      builder: (context, _) {
+        final imageName = widget.loadedImage?.fileName ?? 'No file selected';
+        final sourceSize = widget.loadedImage == null
+            ? '—'
+            : '${widget.loadedImage!.pixelWidth} × ${widget.loadedImage!.pixelHeight}';
 
-    return TitledPanel(
-      title: 'Image Controls',
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          FileLoadRow(
-            buttonText: 'Load',
-            fileName: imageName,
-            onPressed: _handleLoadImage,
-          ),
+        return TitledPanel(
+          title: 'Image Controls',
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              FileLoadRow(
+                buttonText: 'Load',
+                fileName: imageName,
+                onPressed: _handleLoadImage,
+              ),
 
-          InfoValueRow(
-            label: 'Source',
-            value: sourceSize,
-          ),
+              InfoValueRow(
+                label: 'Source',
+                value: sourceSize,
+              ),
 
-          SliderNumberRow(
-            label: 'Zoom', value: _zoom,
-            min: 25, max: 300, defaultValue: 100,
-            divisions: 275, decimals: 0, units: '',
-            onChanged: (value) { setState(() { _zoom = value; }); },
-          ),
+              SliderNumberRow(
+                label: 'Zoom',
+                value: widget.project.zoom,
+                min: 25,
+                max: 300,
+                defaultValue: 100,
+                divisions: 275,
+                decimals: 0,
+                units: '%',
+                onChanged: (value) {
+                  widget.project.zoom = value;
+                },
+              ),
 
-          SliderNumberRow(
-            label: 'Offset X', value: _offsetX,
-            min: -100, max: 100, defaultValue: 0,
-            divisions: 200, decimals: 0, units: '',
-            onChanged: (value) { setState(() { _offsetX = value; }); },
-          ),
+              SliderNumberRow(
+                label: 'Offset X',
+                value: widget.project.imageOffsetX,
+                min: -100,
+                max: 100,
+                defaultValue: 0,
+                divisions: 200,
+                decimals: 0,
+                units: '%',
+                onChanged: (value) {
+                  widget.project.imageOffsetX = value;
+                },
+              ),
 
-          SliderNumberRow(
-            label: 'Offset Y', value: _offsetY,
-            min: -100, max: 100, defaultValue: 0,
-            divisions: 200, decimals: 0, units: '',
-            onChanged: (value) { setState(() { _offsetY = value; }); },
-          ),
+              SliderNumberRow(
+                label: 'Offset Y',
+                value: widget.project.imageOffsetY,
+                min: -100,
+                max: 100,
+                defaultValue: 0,
+                divisions: 200,
+                decimals: 0,
+                units: '%',
+                onChanged: (value) {
+                  widget.project.imageOffsetY = value;
+                },
+              ),
 
-          SliderNumberRow(
-            label: 'Bright', value: _brightness,
-            min: 0, max: 200, defaultValue: 100,
-            divisions: 200, decimals: 0, units: '',
-            onChanged: (value) { setState(() { _brightness = value; }); },
-          ),
+              SliderNumberRow(
+                label: 'Bright',
+                value: widget.project.brightness,
+                min: 0,
+                max: 200,
+                defaultValue: 100,
+                divisions: 200,
+                decimals: 0,
+                units: '%',
+                onChanged: (value) {
+                  widget.project.brightness = value;
+                },
+              ),
 
-          SliderNumberRow(
-            label: 'Contrast', value: _contrast,
-            min: 0, max: 200, defaultValue: 100,
-            divisions: 200, decimals: 0, units: '',
-            onChanged: (value) { setState(() { _contrast = value; }); },
-          ),
+              SliderNumberRow(
+                label: 'Contrast',
+                value: widget.project.contrast,
+                min: 0,
+                max: 200,
+                defaultValue: 100,
+                divisions: 200,
+                decimals: 0,
+                units: '%',
+                onChanged: (value) {
+                  widget.project.contrast = value;
+                },
+              ),
 
-          SliderNumberRow(
-            label: 'Sat', value: _saturation,
-            min: 0, max: 200, defaultValue: 100,
-            divisions: 200, decimals: 0, units: '',
-            onChanged: (value) { setState(() { _saturation = value; }); },
-          ),
+              SliderNumberRow(
+                label: 'Sat',
+                value: widget.project.saturation,
+                min: 0,
+                max: 200,
+                defaultValue: 100,
+                divisions: 200,
+                decimals: 0,
+                units: '%',
+                onChanged: (value) {
+                  widget.project.saturation = value;
+                },
+              ),
 
-          SliderNumberRow(
-            label: 'Gamma', value: _gamma,
-            min: 0.25, max: 3.00, defaultValue: 1.00,
-            divisions: 275, decimals: 2, units: '',
-            onChanged: (value) { setState(() { _gamma = value; }); },
-          ),
+              SliderNumberRow(
+                label: 'Gamma',
+                value: widget.project.gamma,
+                min: 0.25,
+                max: 3.00,
+                defaultValue: 1.00,
+                divisions: 275,
+                decimals: 2,
+                units: '',
+                onChanged: (value) {
+                  widget.project.gamma = value;
+                },
+              ),
 
-          LabeledSwitchRow(
-            label: 'Maintain AR',
-            value: _maintainAspectRatio,
-            onChanged: (value) {
-              setState(() {
-                _maintainAspectRatio = value;
-              });
-            },
+              LabeledSwitchRow(
+                label: 'Maintain AR',
+                value: widget.project.maintainAspectRatio,
+                onChanged: (value) {
+                  widget.project.maintainAspectRatio = value;
+                },
+              ),
+            ],
           ),
-        ],
-      ),
+        );
+      },
     );
   }
 }
